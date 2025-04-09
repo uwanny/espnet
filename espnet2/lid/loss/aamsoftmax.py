Fix softmax losses.

espnet2/lid/loss/aamsoftmax.py
@@ -54,21 +54,22 @@ class AAMSoftmax(AbsLoss):
         print("Initialised AAMSoftmax margin %.3f scale %.3f" % (self.m, self.s))
 
     def forward(self, x, label=None):
-        if len(label.size()) == 2:
-            label = label.squeeze(1)
-
-        assert x.size()[0] == label.size()[0]
         assert x.size()[1] == self.in_feats
 
         # cos(theta)
         # the normalized vector mul, the results is the cosine value
         cosine = F.linear(F.normalize(x), F.normalize(self.weight)) # F.normalize is L2 norm
+        pred_lids = torch.argmax(cosine, dim=1) # (batch,), each is the index of the predicted lid
 
-        pred_lids = torch.argmax(cosine, dim=1)
         if label is not None: # train or dev
+            assert x.size()[0] == label.size()[0]
+            if len(label.size()) == 2:
+                label = label.squeeze(1)
             accuracy = (pred_lids == label).float().mean()
-        else:
+        else: # inference
+            loss = None
             accuracy = None
+            return loss, accuracy, pred_lids
 
         # cos(theta + m)
         sine = torch.sqrt((1.0 - torch.mul(cosine, cosine)).clamp(0, 1))
@@ -83,8 +84,8 @@ class AAMSoftmax(AbsLoss):
         one_hot = torch.zeros_like(cosine)
         one_hot.scatter_(1, label.view(-1, 1), 1)
         # one_hot * phi is for the correct class, which needs margin, 
-        # while (1 - one_hot) * cosine is for the incorrect classes, which don't need margin
-        # output is the added margin logits, for softmax
+        # while (1 - one_hot) * cosine is for the incorrect classes, 
+        # which don't need margin
         output = (one_hot * phi) + ((1.0 - one_hot) * cosine) 
         output = output * self.s
         

espnet2/lid/loss/aamsoftmax_subcenter_intertopk.py
@@ -107,12 +107,16 @@ class ArcMarginProduct_intertopk_subcenter(AbsLoss):
             cosine, (-1, self.out_features, self.K)
         )  # (batch, out_dim, k)
         cosine, _ = torch.max(cosine, 2)  # (batch, out_dim)
+        pred_lids = torch.argmax(cosine, dim=1) # (batch,)
 
-        pred_lids = torch.argmax(cosine, dim=1) # (batch,), each is the index of the predicted lid
         if label is not None:
+            if len(label.size()) == 2:
+                label = label.squeeze(1)
             accuracy = (pred_lids == label).float().mean()
-        else:
+        else: # inference
+            loss = None
             accuracy = None
+            return loss, accuracy, pred_lids
 
         sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
         phi = cosine * self.cos_m - sine * self.sin_m
@@ -126,8 +130,6 @@ class ArcMarginProduct_intertopk_subcenter(AbsLoss):
             phi = torch.where(cosine > self.th, phi, cosine - self.mmm)
             ########
 
-        if len(label.size()) == 2:
-            label = label.squeeze(1)
         one_hot = torch.zeros_like(cosine)
         one_hot.scatter_(1, label.view(-1, 1), 1)
 

espnet2/lid/loss/softmax.py
@@ -26,19 +26,21 @@ class Softmax(AbsLoss):
         nn.init.xavier_normal_(self.weight, gain=1)
 
     def forward(self, x, label=None):
-        if len(label.size()) == 2:
-            label = label.squeeze(1)
-
-        assert x.size()[0] == label.size()[0]
         assert x.size()[1] == self.in_feats
 
         logits = F.linear(F.normalize(x), F.normalize(self.weight))
-        loss = self.ce(logits, label)
-
         pred_lids = torch.argmax(logits, dim=1)
+
         if label is not None:
+            assert x.size()[0] == label.size()[0]
+            if len(label.size()) == 2:
+                label = label.squeeze(1)
             accuracy = (pred_lids == label).float().mean()
         else: # inference
+            loss = None
             accuracy = None
+            return loss, accuracy, pred_lids
+
+        loss = self.ce(logits, label)
 
         return loss, accuracy, pred_lids